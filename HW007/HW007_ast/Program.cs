c5c273a0ae29cdfd21bcaba96f8e31a7ed871626
keizrn <bulavana@yandex.ru>
2022-09-13 00:08:25 +0300

    HW 007 Roman done on test1

--- HW007/HW007_ast/Program.cs
+++ HW007/HW007_ast/Program.cs
@@ -15,13 +15,19 @@
     bool check1 = true;
     bool V1 = false;
     bool X1 = false;
+    bool XL1 = false;
     bool L1 = false;
     bool C1 = false;
+    bool CD1 = false;
     bool D1 = false;
     bool M1 = false;
     bool IV1 = false;
     bool I1 = false;
-    bool XL1 = false;
+
+    int count_I1 = 0;
+    int count_X1 = 0;
+    int count_C1 = 0;
+    int count_M1 = 0;
 
     for (int i = 1; i < count1 + 1; i++)
     {
@@ -31,9 +37,10 @@
                 {
                     arab1 = arab1 + 1;
                     I1 = true;
+                    count_I1++;
                     break;
                 }
-            case ('I') when (((I1 == true) && (V1 == true)) || (IV1 == true) || (L1 == true) || (C1 == true) || (D1 == true) || (M1 == true)):
+            case ('I') when (((I1 == true) && (V1 == true)) || ((I1 == true) && (X1 == true)) || (count_I1 == 3) || (IV1 == true) || (L1 == true) || (C1 == true) || (D1 == true) || (M1 == true)):
                 {
                     check1 = false;
                     return (check1, 0);
@@ -42,9 +49,10 @@
                 {
                     arab1 = arab1 - 1;
                     IV1 = true;
+                    X1 = false;
                     break;
                 }
-            case ('V') when ((V1 == true) || (L1 == true) || (C1 == true) || (D1 == true) || (M1 == true)):
+            case ('V') when ((V1 == true) || (X1 == true) || (L1 == true) || (C1 == true) || (D1 == true) || (M1 == true)):
                 {
                     check1 = false;
                     return (check1, 0);
@@ -56,21 +64,28 @@
                     break;
                 }
             case ('X') when ((L1 == false) && (C1 == false)):
-                    {
-                        arab1 = arab1 + 10;
-                        X1 = true;
-                        break;
-                    }
-            case ('X') when (((X1 == true) && ((L1 == true) || (C1 == true))) || (XL1 == true) || (D1 == true) || (M1 == true)):
-            {
-                check1 = false;
+                {
+                    arab1 = arab1 + 10;
+                    X1 = true;
+                    count_X1++;
+                    break;
+                }
+            case ('X') when (((X1 == true) && ((L1 == true) || (C1 == true))) || (count_X1 == 3) || (XL1 == true) || (D1 == true) || (M1 == true)):
+                {
+                    check1 = false;
                     return (check1, 0);
-            }
+                }
             case ('X'):
-            {
-                arab1 = arab1 - 10;
-                XL1 = true;
-                break;
+                {
+                    arab1 = arab1 - 10;
+                    XL1 = true;
+                    C1 = false;
+                    break;
+                }
+            case ('L') when ((L1 == true) || (C1 == true) || (D1 == true) || (M1 == true)):
+                {
+                    check1 = false;
+                    return (check1, 0);
                 }
             case ('L'):
                 {
@@ -78,25 +93,62 @@
                     L1 = true;
                     break;
                 }
-            case ('C'):
+            case ('C') when ((D1 == false) && (M1 == false)):
                 {
                     arab1 = arab1 + 100;
                     C1 = true;
+                    count_C1++;
                     break;
                 }
+            case ('C') when (((C1 == true) && ((D1 == true) || (M1 == true))) || (count_C1 == 3) || (CD1 == true)):
+                {
+                    check1 = false;
+                    return (check1, 0);
+                }
+            case ('C'):
+                {
+                    arab1 = arab1 - 100;
+                    CD1 = true;
+                    M1 = false;
+                    break;
+                }
+            case ('D') when ((D1 == true) || (M1 == true)):
+                {
+                    check1 = false;
+                    return (check1, 0);
+                }
+            case ('D'):
+                {
+                    arab1 = arab1 + 500;
+                    D1 = true;
+                    break;
+                }
+            case ('M') when (count_M1 == 3):
+                {
+                    check1 = false;
+                    return (check1, 0);
+                }
+            case ('M'):
+                {
+                    arab1 = arab1 + 1000;
+                    M1 = true;
+                    count_M1++;
+                    break;
+                }
+
             default:
                 {
                     check1 = false;
                     return (check1, 0);
-                    break;
+
                 }
 
+
         }
+        //Console.WriteLine(arab1); 
     }
     return (check1, arab1);
 }
-
-
 
 Console.WriteLine("Input any Roman number..");
 string roman0 = Console.ReadLine();
@@ -104,4 +156,3 @@ roman0 = roman0.ToUpper();
 (bool check0, int arabic0) = RomantoArabic1(roman0);
 if (check0 == false) Console.WriteLine("Your input was NOT correct.");
 else Console.WriteLine($"Arabic number is: {arabic0}");
-
