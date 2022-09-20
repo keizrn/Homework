0c21cbec0cc2c26eb6347ce92a6ab7e8895b373c
keizrn <BulavaNA@yandex.ru>
2022-09-20 16:54:13 +0300

    HW009 completed. Asterisk done.

--- HW009/HW009_ast/Program.cs
+++ HW009/HW009_ast/Program.cs
@@ -6,39 +6,57 @@
 
 
 
-(bool, char) FindParentheses(string string1)
+(bool, char, string) FindParentheses(string string1)
 {
-    bool check1 = true;
-    char c1 = string1[0];
-    Console.WriteLine(string1);
-    switch (c1)
+    if (string1 == "") return (true, '\0', "");
+    else
+    {
+        char c1 = string1[0];
+        //Console.WriteLine(string1);
+        switch (c1)
         {
             case ('('):
-            {
-                
-            }
+                {
+                    (bool check2, char c2, string string2) = FindParentheses(string1.Substring(1, string1.Length - 1));
+                    //Console.WriteLine($"c1: {c1} , c2: {c2}");
+                    if ((c2 != (char)(((int)c1) + 1))) return (false, '\0', "");
+                    else return FindParentheses(string2);
+                }
+            case ('['):
+                {
+                    (bool check2, char c2, string string2) = FindParentheses(string1.Substring(1, string1.Length - 1));
+                    //Console.WriteLine($"c1: {c1} , c2: {c2}");
+                    if ((c2 != (char)(((int)c1) + 2))) return (false, '\0', "");
+                    else return FindParentheses(string2);
+                }
+            case ('{'):
+                {
+                    (bool check2, char c2, string string2) = FindParentheses(string1.Substring(1, string1.Length - 1));
+                    //Console.WriteLine($"c1: {c1} , c2: {c2}");
+                    if ((c2 != (char)(((int)c1) + 2))) return (false, '\0', "");
+                    else return FindParentheses(string2);
+                }
+            case (')'):
+                {
+                    return (true, c1, string1.Substring(1, string1.Length - 1));
+                }
+            case (']'):
+                {
+                    return (true, c1, string1.Substring(1, string1.Length - 1));
 
-
-
-    if ((c1 == '(') || (c1 == '[') || (c1 == '{'))
-    {
-        for (int i = 1; i < string1.Length + 1; i++)
-        {
-        (bool check2, char c2) = FindParentheses(string1.Substring(i,string1.Length - i));
-        Console.WriteLine($"c1: {c1} , c2: {c2}");
-        if ((c2 != (char)(((int)c1) + 1)) && (c2 != (char)(((int)c1) + 2))) return (false, '\0');
-        else return (true, c2);
+                }
+            case ('}'):
+                {
+                    return (true, c1, string1.Substring(1, string1.Length - 1));
+                }
+            default:
+                return FindParentheses(string1.Substring(1, string1.Length - 1));
         }
     }
-    else if ((c1 == ')') || (c1 == ']') || (c1 == '}'))
-    {
-        return (true, c1);
-    }
-    return (true, '\0');
 }
 
-Console.WriteLine("Input string");
-string string0 = Console.ReadLine();
-(bool check0, char p0) = FindParentheses(string0);
+Console.WriteLine("Input string with different types of brackets for check..");
+string string0 = Console.ReadLine()!;
+(bool check0, char p0, string S1) = FindParentheses(string0);
 if (!check0) Console.WriteLine("Error");
 else Console.WriteLine("OK");
