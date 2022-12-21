4c3466709c2034bbeda2f3c87df74173bc1aeb9b
keizrn <111270948+keizrn@users.noreply.github.com>
2022-12-21 11:36:42 +0300

    HW3 corrected. 21.12.22

--- Web/HW3/1.js
+++ Web/HW3/1.js
@@ -1,23 +1,20 @@
-
-      
-      
-  Number.prototype.countDecimals = function() {
-    if(Math.floor(this.valueOf()) === this.valueOf()) return 0;
-    return this.toString().split(".")[1].length || 0; 
-  }
+function countDecimals(numDecimal) {
+  if(Math.floor(numDecimal.valueOf()) === numDecimal.valueOf()) return 0;
+  return numDecimal.toString().split(".")[1].length || 0; 
+}
   
-  function cels_To_Far() {
-    flag = true;
-    do { 
-        let cels_1 = parseFloat(prompt("Введите температуру в градусах Цельсия:").replace(',','.'));
-        if (isNaN(cels_1)) {
-          flag = confirm("Неправильно введено число. Посчитать еще раз?");
-        } else {
-          dec = 10**Math.max(cels_1.countDecimals(), 1);
-          far_1 = Math.round(((9 / 5) * cels_1 + 32 + Number.EPSILON) * dec) / dec;
-          alert("Температура " + cels_1 + " в градусах Цельсия эквивалентна " + far_1 + " в градусах Фаренгейта.");
-          flag = confirm("Посчитать еще раз?");
-        }
-    } while (flag == true)
-  }
-  cels_To_Far();
+function celsiumToFahrenheit() {
+  let flag = true;
+  do { 
+    let celsium = Number.parseFloat(prompt("Введите температуру в градусах Цельсия:").replace(',','.'));
+      if (isNaN(celsium)) {
+        flag = confirm("Неправильно введено число. Посчитать еще раз?");
+      } else {
+        let decimalPlaces = 10**Math.max(countDecimals(celsium), 1);
+        fahrenheit = Math.round(((9 / 5) * celsium + 32 + Number.EPSILON) * decimalPlaces) / decimalPlaces;
+        alert("Температура " + celsium + " в градусах Цельсия эквивалентна " + fahrenheit + " в градусах Фаренгейта.");
+        flag = confirm("Посчитать еще раз?");
+      }
+  } while (flag);
+}
+celsiumToFahrenheit();
